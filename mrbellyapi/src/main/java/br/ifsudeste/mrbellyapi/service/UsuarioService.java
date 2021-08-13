378eb15d9474b5ae7b7bb1edd53713dd72a400e7
rennan <rennandamiao@gmail.com>
2021-08-13 08:35:04 -0300

    usuarioService

--- mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/service/UsuarioService.java
+++ mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package br.ifsudeste.mrbellyapi.service;
 
+import br.ifsudeste.mrbellyapi.api.exception.SenhaInvalidaException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -36,4 +37,14 @@ public class UsuarioService implements UserDetailsService {
 
 		return User.builder().username(usuario.getLogin()).password(usuario.getSenha()).roles(roles).build();
 	}
+	public UserDetails autenticar(Usuario usuario){
+		UserDetails user = loadUserByUsername(usuario.getLogin());
+		boolean senhasBatem = encoder.matches(usuario.getSenha(), user.getPassword());
+
+		if (senhasBatem){
+			return user;
+		}
+		throw new SenhaInvalidaException();
+	}
+
 }
