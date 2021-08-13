9c90dae574d72e8bcb72aeeb334ed200bf4bf4fb
rennan <rennandamiao@gmail.com>
2021-08-13 08:46:03 -0300

    Usuario controller

--- mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/api/controller/UsuarioController.java
+++ mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/api/controller/UsuarioController.java
@@ -1,6 +1,12 @@
 package br.ifsudeste.mrbellyapi.api.controller;
 
+import br.ifsudeste.mrbellyapi.api.dto.CredenciaisDTO;
+import br.ifsudeste.mrbellyapi.api.dto.TokenDTO;
+import br.ifsudeste.mrbellyapi.api.exception.SenhaInvalidaException;
+import br.ifsudeste.mrbellyapi.config.JwtService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ifsudeste.mrbellyapi.model.entity.Usuario;
 import br.ifsudeste.mrbellyapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
@@ -19,6 +26,7 @@ public class UsuarioController {
 
 	private final UsuarioService usuarioService;
 	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
@@ -26,5 +34,18 @@ public class UsuarioController {
 		String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
 		usuario.setSenha(senhaCriptografada);
 		return usuarioService.salvar(usuario);
+	}
+	@PostMapping("/auth")
+	public TokenDTO autenticar(@RequestBody CredenciaisDTO credenciais){
+		try{
+			Usuario usuario = Usuario.builder()
+					.login(credenciais.getLogin())
+					.senha(credenciais.getSenha()).build();
+			UserDetails usuarioAutenticado = usuarioService.autenticar(usuario);
+			String token = jwtService.gerarToken(usuario);
+			return new TokenDTO(usuario.getLogin(), token);
+		} catch (UsernameNotFoundException | SenhaInvalidaException e ){
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+		}
 	}
 }
