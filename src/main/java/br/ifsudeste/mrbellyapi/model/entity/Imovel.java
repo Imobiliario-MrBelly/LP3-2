d0ce4f90252e8d5c5879d6bccccb02af867d0620
Vinícius Batista de Vasconcelos <vinivascon04@gmail.com>
2021-08-28 14:39:39 -0300

    Update Imovel.java

--- src/main/java/br/ifsudeste/mrbellyapi/model/entity/Imovel.java
+++ src/main/java/br/ifsudeste/mrbellyapi/model/entity/Imovel.java
@@ -10,6 +10,8 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,6 +30,7 @@ public class Imovel {
 	private double condominio;
 	private double iptu;
 	private int garagem;
+	@JsonIgnore
 	@ManyToOne
 	@JoinTable(name = "imoveis_locador", joinColumns = @JoinColumn(name = "imovel_id"), inverseJoinColumns = @JoinColumn(name = "locador_id"))
 	private Locador locador;
